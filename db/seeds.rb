27567087f809581f8d864d6cd7eba17a569b5908
gat41 <gloria.tengaihui@gmail.com>
2016-10-26 23:19:05 +0800

    complete basic profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Seeding user"
+#later
+
+puts "Seeding histories"
+
+
+puts "Seeding facilities"
+
